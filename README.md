ec207cd2ae3d63127e4509eb3ff3100ea547202d
Pyaesone Khant <pyaepsk@gmail.com>
2023-02-19 17:35:25 +0630

    First NPM package

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # show-my-name-psk
+
+> npm i show-my-name-psk
+
+
+# usage
+
+* import {myName, showMyName, showMyNameUI} from 'show-my-name-psk';
---
+++ index.js
@@ -0,0 +1,12 @@
+export const myName = "Pyaesone Khant";
+export const showMyName = () => {
+    return myName;
+}
+
+export const showMyNameUI = () => {
+    const h1 = document.createElement("h1");
+    h1.innerText = myName;
+    document.body.prepend(h1);
+}
+
+showMyNameUI();
